872f3a6bb93a59b86c9387ac975f7b647e80f2e2
priya <priyapandey8884.blr@gmail.com>
2022-09-07 23:13:58 +0530

    ADDED:[Priya] Display Welcome Message

---
+++ UserRegistration/src/com/bridgelabz/UserRegistrationMain.java
@@ -0,0 +1,14 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+/**
+ * to display welcome message
+ */
+public class UserRegistrationMain {
+    public static void main(String[] args){
+        System.out.println("Welcome to the User Registration System");
+
+
+    }
+}
