d8ac0b0a69181c96593d634f251736e55cb0c1dc
priya <priyapandey8884.blr@gmail.com>
2022-09-08 00:18:16 +0530

    ADDED:[Priya] Ability to check the validation for last name

--- UserRegistration/src/com/bridgelabz/UserRegistration.java
+++ UserRegistration/src/com/bridgelabz/UserRegistration.java
@@ -6,26 +6,26 @@ import java.util.regex.Pattern;
 public class UserRegistration {
     /**
      *
-     * @param FirstName:input from the user
+     * @param LastName:input from the user
      */
 
-    public static void toCheckForFirstNameValidation(String FirstName){
+    public static void toCheckForLastNameValidation(String LastName){
         /**
-         * regex pattern is taken;where starting should be Capital letter and then min of 3 characters can be taken
+         * regex pattern is taken;where Ending should be Capital letter and then min of 3 characters can be taken
          * pattern is then matched with user input name by using matcher class
          * if the  regex matches with the input name,then print has valid name
          * else invalid username
          */
-        boolean isFirstName;
-        String regexName = "^[A-Z]{1}[a-z A-Z]{3,}$";
+        boolean isLastName;
+        String regexName = "^[A-Z]{1}[A-Z a-z]{3,}$";
         Pattern object = Pattern.compile(regexName);
-        Matcher objectMatcher = object.matcher(FirstName);
-        isFirstName = objectMatcher.matches();
-        if(isFirstName) {
-            System.out.println(FirstName + " is an valid name");
+        Matcher objectMatcher = object.matcher(LastName);
+        isLastName = objectMatcher.matches();
+        if(isLastName) {
+            System.out.println(LastName + " is an valid name");
         }
         else{
-            System.out.println(FirstName + " is an invalid user name");
+            System.out.println(LastName + " is an invalid user name");
         }
     }
 }
